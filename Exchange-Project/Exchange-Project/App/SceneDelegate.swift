304c719ea84729e0a65782a6b9c041e493f3ace0
seungchan <chanchu@hansung.ac.kr>
2023-12-12 16:06:06 +0900

    [#1] App

---
+++ Exchange-Project/Exchange-Project/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Exchange-Project
+//
+//  Created by 김승찬 on 2023/12/12.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigationController = UINavigationController(rootViewController: ExchangeRateViewController())
+        
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+    }
+}
+
