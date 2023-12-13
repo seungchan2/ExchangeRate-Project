52310be84aeb9969921a0e69e721b96f601f5fa0
seungchan <chanchu@hansung.ac.kr>
2023-12-13 19:34:37 +0900

    [#5] Double+

---
+++ Exchange-Project/Exchange-Project/Util/Double +.swift
@@ -0,0 +1,14 @@
+//
+//  Double +.swift
+//  Exchange-Project
+//
+//  Created by 김승찬 on 2023/12/13.
+//
+
+import Foundation
+
+extension Double {
+    func formattedString() -> String {
+        return String(format: "%.2f", self)
+    }
+}
